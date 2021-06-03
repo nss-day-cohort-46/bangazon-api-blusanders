83a39bfa448df9c77984f9bf43afc6c5f27ebd18
blusanders <blusanders@yahoo.com>
2021-06-03 15:20:34 -0500

    Delete payment type test

--- tests/payments.py
+++ tests/payments.py
@@ -2,7 +2,7 @@ import datetime
 import json
 from rest_framework import status
 from rest_framework.test import APITestCase
-
+from bangazonapi.models.payment import Payment
 
 class PaymentTests(APITestCase):
     def setUp(self) -> None:
@@ -41,3 +41,22 @@ class PaymentTests(APITestCase):
         self.assertEqual(json_response["create_date"], str(datetime.date.today()))
 
     # TODO: Delete payment type
+
+    def test_delete_payment_type(self):
+        """
+        Ensure we can delete a payment type for a customer
+        """
+        payment = Payment()
+        payment.merchant_name = "American Express"
+        payment.account_number = "111-1111-1111"
+        payment.expiration_date = "2024-12-31"
+        payment.create_date = "datetime.date.today()"
+        payment.save()
+
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token)
+        response = self.client.delete(f"/paymenttypes/{payment.id}")
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token)
+        response = self.client.get(f"/paymenttypes/{payment.id}")
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
