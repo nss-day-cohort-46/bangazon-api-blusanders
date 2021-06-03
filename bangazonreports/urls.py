4bc3cbd7350d9c20906012141b59e43d1ca883e3
blusanders <blusanders@yahoo.com>
2021-06-02 20:35:35 -0500

    Incomplete order report

--- bangazonreports/urls.py
+++ bangazonreports/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import completed_orders_list
+from .views import incomplete_orders_list
 
 urlpatterns = [
     path('reports/orderscompleted', completed_orders_list),
+    path('reports/ordersincomplete', incomplete_orders_list),
 ]
--- bangazonreports/views/__init__.py
+++ bangazonreports/views/__init__.py
@@ -1,2 +1,3 @@
 from .connection import Connection
 from .orders.completed_orders import completed_orders_list
+from .orders.incomplete_orders import incomplete_orders_list
