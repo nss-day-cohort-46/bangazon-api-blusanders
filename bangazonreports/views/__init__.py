05623a273d328578dd31615c6b478c1acb4dc4e2
blusanders <blusanders@yahoo.com>
2021-06-04 10:57:03 -0500

    Expensive Product Report

--- bangazonreports/views/__init__.py
+++ bangazonreports/views/__init__.py
@@ -2,3 +2,4 @@ from .connection import Connection
 from .orders.completed_orders import completed_orders_list
 from .orders.incomplete_orders import incomplete_orders_list
 from .users.users_fav import user_favorites_list
+from .prod.prod_expensive import expensive_product_list
--- bangazonreports/views/orders/completed_orders.py
+++ bangazonreports/views/orders/completed_orders.py
@@ -3,46 +3,13 @@ import sqlite3
 from django.shortcuts import render
 from bangazonapi.models import Order
 from bangazonreports.views import Connection
-
+from django.db.models import Sum
 
 def completed_orders_list(request):
 
     if request.method == 'GET':
 
-        with sqlite3.connect(Connection.db_path) as conn:
-            conn.row_factory = sqlite3.Row
-            db_cursor = conn.cursor()
-
-            #get all completed orders
-            db_cursor.execute("""
-                    SELECT
-                        o.*,
-                        u.first_name, u.last_name,
-                        py.merchant_name,
-                        sum(p.price) as totalPrice
-                    FROM bangazonapi_order o
-                    JOIN bangazonapi_customer c ON o.customer_id = c.id
-                    JOIN bangazonapi_orderproduct op on op.order_id = o.id 
-                    JOIN bangazonapi_product p on op.product_id = p.id 
-                    JOIN bangazonapi_payment py on py.id = o.payment_type_id
-                    JOIN auth_user u ON c.user_id = u.id
-
-                    where payment_type_id is not NULL
-                    group by c.id
-                """)
-            dataset = db_cursor.fetchall()
-
-            completed_orders_list = []
-
-            for row in dataset:
-                completed_orders = {}
-                completed_orders["id"] = row["id"]
-                completed_orders["first_name"] = row["first_name"]
-                completed_orders["last_name"] = row["last_name"]
-                completed_orders["merchant_name"] = row["merchant_name"]
-                completed_orders["total_price"] = row["totalPrice"]
-
-                completed_orders_list.append(completed_orders)
+        completed_orders_list = Order.objects.filter(payment_type__isnull=False, lineitems__isnull=False).annotate(total_price=Sum('lineitems__product__price'))
 
         template = 'orders/completed_orders.html'
         context = {
